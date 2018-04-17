0a0cbc2fcecd12aa75fc1bc7a577b04063fed8df
Dan Smith <dsmith@danplanet.com>
2018-04-16 18:40:49 -0700

    Call this 0.5.2 because I'm an idiot

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = list(val.strip() for val in open('requirements.txt'))
 tests_require = list(val.strip() for val in open('test_requirements.txt'))
 
 setup(name='somecomfort',
-      version='0.6.0',
+      version='0.5.2',
       description='A client for Honeywell\'s US-based cloud devices',
       author='Dan Smith',
       author_email='dsmith+somecomfort@danplanet.com',
